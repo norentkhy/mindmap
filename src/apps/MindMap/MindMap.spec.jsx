16045662702a6e9d7ccdd770a4ab9246006f788d
Norent Khy <norent.dev@gmail.com>
2021-03-11 17:48:20 +0100

    MindMap: Integrate "rename a tab" feature
    - This was actually already integrated
    - But now an integration test is added

--- src/apps/MindMap/MindMap.spec.jsx
+++ src/apps/MindMap/MindMap.spec.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import { MindMap } from './MindMap';
+import userEvent from '@testing-library/user-event';
 
 describe('view elements', () => {
   test('tabs', () => {
@@ -29,5 +30,17 @@ describe('tabs integration', () => {
 
     fireEvent.click(screen.getByLabelText('add new tab'));
     expect(screen.getAllByText('untitled').length).toBe(2);
+  });
+
+  test('rename a tab', () => {
+    render(<MindMap />);
+    fireEvent.click(screen.getByLabelText('add new tab'));
+    fireEvent.doubleClick(screen.getByText('untitled'));
+
+    const someNewTitle = 'some new title';
+    userEvent.type(document.activeElement, someNewTitle);
+    userEvent.type(document.activeElement, '{enter}');
+
+    expect(screen.getByText(someNewTitle)).toBeVisible();
   });
 });
