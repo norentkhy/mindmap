0f2109d3c87d8716d7298e6ab0f568ab15d47f0e
Norent Khy <norent.dev@gmail.com>
2021-04-13 14:18:41 +0200

    Refactor verbose log for readability

--- src/test-utils/logging.js
+++ src/test-utils/logging.js
@@ -1,19 +1,19 @@
 import { inspect } from 'util'
 
-export function logInputAndOutputOf(fn) {
+export function logInputAndOutputOf(targetFunction) {
   return functionWithLogs
 
-  function functionWithLogs(...args) {
-    console.log(`log event of ${fn}`)
-
-    console.log(`input: ${args}`)
-    const value = fn(...args)
-    console.log(`output: ${value}`)
-
-    return value
+  function functionWithLogs(...input) {
+    const output = targetFunction(...input)
+    logVerbose({ targetFunction, input, output })
+    return output
   }
 }
 
 export function logVerbose(value) {
-  console.log(inspect(value, { showHidden: false, depth: null }))
+  console.log(getVerboseForm(value))
+}
+
+function getVerboseForm(obj) {
+  return inspect(obj, { showHidden: false, depth: null })
 }
